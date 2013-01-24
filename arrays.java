95117961c43a9c6d33aef4677e80bb69768933db
Sean Steele <steele.sean303@gmail.com>
2013-01-24 08:42:05 -0700

    Made sum into a new method

--- arrays.java
+++ arrays.java
@@ -3,16 +3,21 @@ public class arrays {
 	public static void main(String[] args) {
 		int[] nums = {3,6,4,3,2,1,5,6};
 
-		System.out.println(avg(nums));
+		System.out.println(avg(sum(nums), nums));
 	}
 
-	public static double avg(int[] nums){
+	public static double avg(double x, int[] nums){
+		
+		return  x/nums.length;
+		
+	}
+
+	public static double sum(int[] nums){
 		double x = 0;
 		for(int i = 0; i < nums.length; i++){
 			x += nums[i];
 		}
-		return x/nums.length;
-		
+		return x;
 	}
 
 }
