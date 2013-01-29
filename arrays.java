9cad107842c19442c723ee81253026576e43af68
Sean Steele <steele.sean303@gmail.com>
2013-01-29 07:53:21 -0700

    added block comments to methods

--- arrays.java
+++ arrays.java
@@ -1,4 +1,14 @@
+/**
+ * arrays
+ *
+ * 		Methods for dealing with arrays
+ *
+ * Sean Steele
+ */
+
 public class arrays {
+
+
 
 	public static void main(String[] args) {
 		int[] nums = {3,6,4,3,2,1,5,6};
@@ -6,12 +16,27 @@ public class arrays {
 		System.out.println(avg(sum(nums), nums));
 	}
 
+	/**
+	 * avg
+	 * 		Finds the mean of an array of ints
+	 *
+	 * @param nums - the array
+	 * @return the mean
+	 */
 	public static double avg(double x, int[] nums){
 		
 		return  x/nums.length;
 		
 	}
 
+
+	/**
+	 * sum
+	 * 		Finds the sum of an array of ints
+	 *
+	 * @param nums - the array of ints
+	 * @return the sum
+ 	 */
 	public static double sum(int[] nums){
 		double x = 0;
 		for(int i = 0; i < nums.length; i++){
