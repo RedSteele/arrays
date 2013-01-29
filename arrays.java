51258ff84724a0a5ff185d63097e93090d921f20
Sean Steele <steele.sean303@gmail.com>
2013-01-29 08:12:49 -0700

    Made a method to find the number of threes in the array

--- arrays.java
+++ arrays.java
@@ -14,6 +14,7 @@ public class arrays {
 		int[] nums = {3,6,4,3,2,1,5,6};
 
 		System.out.println(avg(sum(nums), nums));
+		System.out.println(findThrees(nums));
 	}
 
 	/**
@@ -43,6 +44,19 @@ public class arrays {
 			x += nums[i];
 		}
 		return x;
+	}
+
+	/**
+	 * 
+	 */
+	public static int findThrees(int[] nums){
+		int tres = 0;
+		for(int i = 0; i < nums.length; i++){
+			if (nums[i] == 3){
+				tres++;
+			}
+		}
+		return tres;
 	}
 
 }
